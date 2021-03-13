9bd9764a6f7fd637c02f407b80971144959a0858
idk <hankhill19580@gmail.com>
2021-03-13 15:44:11 -0500

    Set it up so that the Unix version of i2pconfig will be used if i2pconfig is installed in /usr/local/bin and the ~/.i2p/router.config is present. Does not work for service installs yet.

--- src/unix/i2pconfig.sh
+++ src/unix/i2pconfig.sh
@@ -4,12 +4,23 @@ if [ -f "/etc/i2pbrowser/i2pbrowserrc" ]; then
   . /etc/i2pbrowser/i2pbrowserrc
 fi
 
+if [ -f "$HOME/.i2p/router.config" ]; then
+  if [ "$0" = "/usr/local/bin/i2pconfig" ]; then
+    if ! grep -R 'routerconsole.browser' "$HOME/.i2p/router.config" ; then
+      echo "routerconsole.browser=$0" | tee -a "$HOME/.i2p/router.config"
+    fi
+  fi
+fi
+
 if [ -z $CONFIGURING_PROFILE ]; then
   CONFIGURING_PROFILE="."
 fi
 
 if [ -z $ROUTER_CONSOLE ]; then
-  ROUTER_CONSOLE="http://127.0.0.1:7657"
+  ROUTER_CONSOLE="$1"
+  if [ -z $1 ]; then
+    ROUTER_CONSOLE="http://127.0.0.1:7657"
+  fi
 fi
 
 if [ ! -d "$CONFIGURING_PROFILE" ]; then
