94feece52899062d33cd0f3aaf9fe18fb9d1d5e7
idk <hankhill19580@gmail.com>
2021-08-02 17:20:33 -0400

    Use the copy of the profile from inside the %programfiles%/i2p

--- src/win/i2pbrowser-private.bat
+++ src/win/i2pbrowser-private.bat
@@ -8,10 +8,10 @@ if exist "%ProgramFiles%\I2P\jpackaged" (
 
 if exist "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\" (
   echo "profile is configured, updating extensions"
-  xcopy /s /i /y "%ProgramFiles%\I2PBrowser-Launcher\firefox.profile.i2p\extensions" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\extensions"
+  xcopy /s /i /y "%ProgramFiles%\I2P\I2PBrowser-Launcher\firefox.profile.i2p\extensions" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\extensions"
 ) else (
   echo "configuring profile"
-  xcopy /s /i /y "%ProgramFiles%\I2PBrowser-Launcher\firefox.profile.i2p" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p"
+  xcopy /s /i /y "%ProgramFiles%\I2P\I2PBrowser-Launcher\firefox.profile.i2p" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p"
 )
 
 if exist "%USERPROFILE%/Desktop/Tor Browser/Browser/firefox.exe" (
