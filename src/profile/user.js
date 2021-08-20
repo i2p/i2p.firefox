7ff4eb5e371d7eebff6246b673620675a6842d25
idk <hankhill19580@gmail.com>
2021-08-19 23:54:37 -0400

    Set autodisabledscopes to 0

--- src/profile/user.js
+++ src/profile/user.js
@@ -170,7 +170,7 @@ user_pref("dom.serviceWorkers.enabled", false);
 user_pref("dom.serviceWorkers.interception.enabled", false);
 user_pref("dom.storage.enabled", false);
 user_pref("dom.webaudio.enabled", false);
-user_pref("extensions.autoDisableScopes", 14);
+user_pref("extensions.autoDisableScopes", 0);
 user_pref("extensions.getAddons.cache.enabled", false);
 user_pref("extensions.getAddons.showPane", false);
 user_pref("extensions.pocket.enabled", false);
