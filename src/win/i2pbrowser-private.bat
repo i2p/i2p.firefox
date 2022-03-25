74d5ccc47f40a1ce199fdbcd77e9b7fd80665177
idk <hankhill19580@gmail.com>
2022-03-25 18:51:11 -0400

    cd before starting a jpackaged router from the script in case the environment is overriding it

--- src/win/i2pbrowser-private.bat
+++ src/win/i2pbrowser-private.bat
@@ -15,6 +15,7 @@ if exist "%LocalAppData%\I2P\I2P.exe" (
 )
 
 if exist "%I2PPath%\jpackaged" (
+  cd "%LOCALAPPDATA%\I2P"
   start "i2p" /D "%LOCALAPPDATA%\I2P" "%I2PPath%\i2p.exe"
 ) else (
   start "i2p" "%I2PPath%\i2p.exe"
