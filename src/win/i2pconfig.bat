8135506213e25a8cf169152a5a9a414d0b857355
idk <hankhill19580@gmail.com>
2022-03-25 17:23:49 -0400

    update config launcher script

--- src/win/i2pconfig.bat
+++ src/win/i2pconfig.bat
@@ -1,11 +1,25 @@
-@echo off
+@echo on
+
+set "I2PPath=%ProgramFiles%\I2P\"
+if exist "%ProgramFiles%\I2P\" (
+  set "I2PPath=%ProgramFiles%\I2P\"
+)
+
+if exist "%ProgramFiles(x86)%\I2P" (
+  set "I2PPath=%ProgramFiles(x86)%\I2P"
+)
+
+set "I2PPath=%LocalAppData%\I2P\"
+if exist "%LocalAppData%\I2P\I2P.exe" (
+  set "I2PPath=%LocalAppData%\I2P\"
+)
 
 if exist "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.config.i2p\" (
   echo "profile is configured"
-  xcopy /s /i /y "%ProgramFiles%\I2P\I2PBrowser-Launcher\firefox.profile.i2p\extensions" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\extensions"
+  xcopy /s /i /y "%I2PPath%\I2PBrowser-Launcher\firefox.profile.i2p\extensions" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\extensions"
 ) else (
   echo "configuring profile"
-  xcopy /s /i /y "%ProgramFiles%\I2P\I2PBrowser-Launcher\firefox.profile.config.i2p" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.config.i2p"
+  xcopy /s /i /y "%I2PPath%\I2PBrowser-Launcher\firefox.profile.config.i2p" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.config.i2p"
 )
 
 if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" (
