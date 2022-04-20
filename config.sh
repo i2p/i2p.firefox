ae02a1c6dbf4f9a242fbeaccbd6239db9352e0f3
idk <hankhill19580@gmail.com>
2022-04-20 16:17:58 -0400

    Document how to switch JVM's in config file

--- config.sh
+++ config.sh
@@ -1,21 +1,24 @@
 
-#If you need to use a different JVM, JDK, or other utility from
-#build.sh, set it in this file, for example:
-#PATH="$PATH:/c/Program Files/Java/jdk-17.0.3/bin/"
-#JAVA_HOME="/c/Program Files/Java/jdk-17.0.3"
-#to use it for Oracle OpenJDK17
+## If you need to use a different JVM, JDK, or other utility from
+# build.sh, set it in this file, for example:
 
-#Other potential values:
-#PATH="$PATH:/c/Program Files/OpenJDK/jdk-17.0.3/bin/"
-#JAVA_HOME="/c/Program Files/OpenJDK/jdk-17.0.3"
-#PATH="$PATH:/c/Program Files/Java/jdk-18.0.1/bin/"
-#JAVA_HOME="/c/Program Files/Java/jdk-18.0.1"
-#PATH="$PATH:/c/Program Files/Eclipse Adoptium/jdk-17.0.3/bin/"
-#JAVA_HOME="/c/Program Files/Eclipse Adoptium/jdk-17.0.3"
-#Which will determine, of course, which java compilers you use and where
-#your JAVA_HOME(and thus your bootclasspath jars and stuff) come from.
-#So for you reddit nerds who are all into graalVM or whatever, this might
-#be where you go to mess with that.
+#export PATH="$PATH:/c/Program Files/Java/jdk-17.0.3/bin/"
+#export JAVA_HOME="/c/Program Files/Java/jdk-17.0.3"
+# to use it for Oracle OpenJDK17
+
+## Other potential values:
+
+#export PATH="$PATH:/c/Program Files/OpenJDK/jdk-17.0.3/bin/"
+#export JAVA_HOME="/c/Program Files/OpenJDK/jdk-17.0.3"
+#export PATH="$PATH:/c/Program Files/Java/jdk-18.0.1/bin/"
+#export JAVA_HOME="/c/Program Files/Java/jdk-18.0.1"
+#export PATH="$PATH:/c/Program Files/Eclipse Adoptium/jdk-17.0.3/bin/"
+#export JAVA_HOME="/c/Program Files/Eclipse Adoptium/jdk-17.0.3"
+
+# Which will determine, of course, which java compilers you use and where
+# your JAVA_HOME(and thus your bootclasspath jars and stuff) come from.
+# So for you reddit nerds who are all into graalVM or whatever, this might
+# be where you go to mess with that.
 
 ## Until 1.7.4, releases were built using Eclipse Adoptium OpenJDK
 # but it lags behind a day or two in security updates which did become
@@ -23,3 +26,9 @@
 # JDK distribution which was more up to date. Should you prefer, or simply
 # wish to experiment with a different JVM, copy this file to `config_overrides.sh`
 # and add your JAVA_HOME and $PATH changes.
+
+# You can also use this to temporarily add applications into the PATH that are
+# required to build this if you do not wish to edit your PATH across the entire
+# Windows session, and for setting ANT_HOME
+#export ANT_HOME="/c/apache-ant-1.10.9"
+#export PATH="$PATH:$ANT_HOME/bin/"
