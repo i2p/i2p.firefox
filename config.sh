c14287048f43ffed1affbbcac65547d03e1d43e4
idk <hankhill19580@gmail.com>
2022-04-20 14:09:40 -0400

    add a config.sh file to configure JVM/JDK at build time

---
+++ config.sh
@@ -0,0 +1,6 @@
+
+#If you need to use a different JVM, JDK, or other utility from
+#build.sh, set it in this file, for example:
+#PATH="$PATH:/c/Program Files/OpenJDK/jdk-17.0.2/bin/"
+#JAVA_HOME="/c/Program Files\OpenJDK\jdk-17.0.2"
+#to use it for Oracle OpenJDK17
