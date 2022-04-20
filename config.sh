115805b9fb509d2318a66c47ccafc4759efe228a
idk <hankhill19580@gmail.com>
2022-04-20 15:53:00 -0400

    Make config.sh point to 17.0.3

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 
 #If you need to use a different JVM, JDK, or other utility from
 #build.sh, set it in this file, for example:
-#PATH="$PATH:/c/Program Files/OpenJDK/jdk-17.0.2/bin/"
-#JAVA_HOME="/c/Program Files\OpenJDK\jdk-17.0.2"
+#PATH="$PATH:/c/Program Files/OpenJDK/jdk-17.0.3/bin/"
+#JAVA_HOME="/c/Program Files\OpenJDK\jdk-17.0.3"
 #to use it for Oracle OpenJDK17
