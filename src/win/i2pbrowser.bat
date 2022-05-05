9305301bf5925a209c413b29b8416d86a5768c2f
idk <hankhill19580@gmail.com>
2022-05-05 16:21:20 -0400

    Fix I2P router detection priorities in Windows launcher scripts #17

--- src/win/i2pbrowser.bat
+++ src/win/i2pbrowser.bat
@@ -1,17 +1,16 @@
 @echo on
 
 set "I2PPath=%ProgramFiles%\I2P\"
+if exist "%LocalAppData%\I2P\I2P.exe" (
+  set "I2PPath=%LocalAppData%\I2P\"
+)
+
 if exist "%ProgramFiles%\I2P\" (
   set "I2PPath=%ProgramFiles%\I2P\"
 )
 
 if exist "%ProgramFiles(x86)%\I2P" (
   set "I2PPath=%ProgramFiles(x86)%\I2P"
-)
-
-set "I2PPath=%LocalAppData%\I2P\"
-if exist "%LocalAppData%\I2P\I2P.exe" (
-  set "I2PPath=%LocalAppData%\I2P\"
 )
 
 if exist "%I2PPath%\jpackaged" (
--- src/win/i2pconfig.bat
+++ src/win/i2pconfig.bat
@@ -1,17 +1,16 @@
 @echo on
 
 set "I2PPath=%ProgramFiles%\I2P\"
+if exist "%LocalAppData%\I2P\I2P.exe" (
+  set "I2PPath=%LocalAppData%\I2P\"
+)
+
 if exist "%ProgramFiles%\I2P\" (
   set "I2PPath=%ProgramFiles%\I2P\"
 )
 
 if exist "%ProgramFiles(x86)%\I2P" (
   set "I2PPath=%ProgramFiles(x86)%\I2P"
-)
-
-set "I2PPath=%LocalAppData%\I2P\"
-if exist "%LocalAppData%\I2P\I2P.exe" (
-  set "I2PPath=%LocalAppData%\I2P\"
 )
 
 if exist "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.config.i2p\" (
