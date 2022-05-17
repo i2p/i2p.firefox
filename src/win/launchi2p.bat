cf091c9e1062fe83c966c8bb49c580a671d66ec8
idk <hankhill19580@gmail.com>
2022-05-17 16:43:41 -0400

    Don't launch I2P if it's already running

--- src/win/launchi2p.bat
+++ src/win/launchi2p.bat
@@ -2,9 +2,16 @@
 SET MYPATH=%~dp0
 call %MYPATH%common.bat
 
-if exist "%I2PPath%\jpackaged" (
-  cd "%LOCALAPPDATA%\I2P"
-  start "i2p" /D "%LOCALAPPDATA%\I2P" "%I2PPath%\i2p.exe"
-) else (
-  start "i2p" "%I2PPath%\i2p.exe"
+::only launch I2P if the proxy is not up on 4444
+netstat /o /a | find /i "listening" | find ":4444" >nul 2>nul && (
+  echo "I2P is already running, not launching"
+) || (
+  if exist "%I2PPath%\jpackaged" (
+    cd "%LOCALAPPDATA%\I2P"
+    start "i2p" /D "%LOCALAPPDATA%\I2P" "%I2PPath%\i2p.exe"
+  ) else (
+    start "i2p" "%I2PPath%\i2p.exe"
+  )
 )
+
+
