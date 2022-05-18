5d9cd3b669f55531650a292cab545b9b514b05e4
idk <hankhill19580@gmail.com>
2022-05-18 16:12:09 -0400

    Execute if statement outcomes on one line because batch.

--- src/win/launchi2p.bat
+++ src/win/launchi2p.bat
@@ -7,7 +7,7 @@ netstat /o /a | find /i "listening" | find ":4444" >nul 2>nul && (
   echo "I2P is already running, not launching"
 ) || (
   if exist "%I2PPath%\jpackaged" (
-    cd "%LOCALAPPDATA%\I2P" && start "i2p" /D "%LOCALAPPDATA%\I2P" "%I2PPath%\i2p.exe"
+    cd "%LOCALAPPDATA%\I2P" & start "i2p" /D "%LOCALAPPDATA%\I2P" "%I2PPath%\i2p.exe"
   ) else (
     start "i2p" "%I2PPath%\i2p.exe"
   )
