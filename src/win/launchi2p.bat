dbc8376ebddbc868f13a5b9f46de0f54953d15c7
idk <hankhill19580@gmail.com>
2022-05-18 16:55:29 -0400

    more batch quoting madness

--- src/win/launchi2p.bat
+++ src/win/launchi2p.bat
@@ -3,6 +3,7 @@
 SET MYPATH=%~dp0
 call "%MYPATH%common.bat"
 
+echo "check if I2P is already running"
 ::only launch I2P if the proxy is not up on 4444
 netstat /o /a | find /i "listening" | find ":4444" >nul 2>nul && (
   echo "I2P is already running, not launching"
