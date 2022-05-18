1b2985665afdd5f624c529b0a741206cf428d2d6
idk <hankhill19580@gmail.com>
2022-05-18 15:59:04 -0400

    Execute if statement outcomes on one line because batch.

--- src/win/copy-profile.bat
+++ src/win/copy-profile.bat
@@ -3,11 +3,9 @@ SET MYPATH=%~dp0
 call "%MYPATH%common.bat"
 
 if (exist "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\") (
-  echo "profile is configured, updating extensions"
-  xcopy /s /i /y "%I2PData%\I2PBrowser-Launcher\firefox.profile.i2p\extensions" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\extensions"
+  echo "profile is configured, updating extensions" & xcopy /s /i /y "%I2PData%\I2PBrowser-Launcher\firefox.profile.i2p\extensions" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\extensions"
 ) else (
-  echo "configuring profile"
-  xcopy /s /i /y "%I2PData%\I2PBrowser-Launcher\firefox.profile.i2p" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p"
+  echo "configuring profile" & xcopy /s /i /y "%I2PData%\I2PBrowser-Launcher\firefox.profile.i2p" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p"
 )
 
 xcopy /s /i /y "%I2PData%\I2PBrowser-Launcher\firefox.profile.i2p\user.js" "%LOCALAPPDATA%\I2PBrowser-Launcher\firefox.profile.i2p\user.js"
