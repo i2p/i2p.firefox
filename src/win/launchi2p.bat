3fda252a34d37d4e0da7c137a906d073a62a1b27
idk <hankhill19580@gmail.com>
2022-05-18 17:06:56 -0400

    more batch quoting madness

--- src/win/launchi2p.bat
+++ src/win/launchi2p.bat
@@ -8,11 +8,7 @@ echo "check if I2P is already running"
 netstat /o /a | find /i "listening" | find ":4444" >nul 2>nul && (
   echo "I2P is already running, not launching"
 ) || (
-  if exist "%I2PPath%jpackaged" (
-    cd "%LOCALAPPDATA%\I2P" & start "i2p" /D "%LOCALAPPDATA%\I2P" "%I2PPath%i2p.exe"
-  ) else (
-    start "i2p" "%I2PPath%i2p.exe"
-  )
+  start "i2p" /D "%I2PData%" "%I2PPath%i2p.exe"
 )
 
 
