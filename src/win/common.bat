7fb9fd4e8db2cff740b6eef399390f46d84cb298
idk <hankhill19580@gmail.com>
2022-05-18 16:28:39 -0400

    Cleanup quoting in common.bat

--- src/win/common.bat
+++ src/win/common.bat
@@ -2,17 +2,17 @@
 SET MYPATH=%~dp0
 
 set ProgramFiles64="%ProgramFiles: (x86)=%"
-set "I2PData=%LocalAppData%\I2P\"
+set I2PData="%LocalAppData%\I2P\"
 
-set "I2PPath=%ProgramFiles%\I2P\"
+set I2PPath="%ProgramFiles%\I2P\"
 if exist "%LocalAppData%\I2P\I2P.exe" (
-  set "I2PPath=%LocalAppData%\I2P\"
+  set I2PPath="%LocalAppData%\I2P\"
 )
 
 if exist "%ProgramFiles64%\I2P\" (
-  set "I2PPath=%ProgramFiles64%\I2P\"
+  set I2PPath="%ProgramFiles64%\I2P\"
 )
 
 if exist "%ProgramFiles(x86)%\I2P" (
-  set "I2PPath=%ProgramFiles(x86)%\I2P"
+  set I2PPath="%ProgramFiles(x86)%\I2P"
 )
