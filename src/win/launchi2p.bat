c10cf6aaeb8a967586ae17d9459205446bc96a14
idk <hankhill19580@gmail.com>
2022-05-18 18:06:12 -0400

    use netstat oan and findstr in router up check because it's magnitudes faster

--- src/win/launchi2p.bat
+++ src/win/launchi2p.bat
@@ -5,7 +5,7 @@ call "%MYPATH%common.bat"
 
 echo "check if I2P is already running"
 ::only launch I2P if the proxy is not up on 4444
-netstat /o /a | find /i "listening" | find ":4444" >nul 2>nul && (
+netstat /o /a /n | findstr "LISTENING" | findstr "4444" >nul 2>nul && (
   echo "I2P is already running, not launching"
 ) || (
   echo "I2P is not running, launching"
