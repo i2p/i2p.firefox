59a1a3d9e644aeeaf3d46a775745c761aa0c791a
idk <hankhill19580@gmail.com>
2022-09-03 21:42:39 -0400

    update browser wrapping bundle

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ if [ ! -f "$HERE/build/jna-platform.jar" ]; then
 fi
 
 if [ ! -f "$HERE/build/i2pfirefox.jar" ]; then
-  wget -O "$HERE/build/i2pfirefox.jar" https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.20/i2pfirefox.jar
+  wget -O "$HERE/build/i2pfirefox.jar" https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.21/i2pfirefox.jar
 fi
 
 cd java
