a230a49af713a34997680602130f13b4cd5b0645
idk <hankhill19580@gmail.com>
2022-09-03 21:17:44 -0400

    format everything for readability and so the brackets all line up again. All whitespace changes.

--- java/net/i2p/router/WinUpdateProcess.java
+++ java/net/i2p/router/WinUpdateProcess.java
@@ -1,80 +1,88 @@
 package net.i2p.router;
 
-import net.i2p.router.*;
-import net.i2p.I2PAppContext;
-
-import java.util.function.*;
 import java.io.*;
+import java.util.function.*;
+import net.i2p.I2PAppContext;
+import net.i2p.router.*;
 import net.i2p.util.Log;
 
 class WinUpdateProcess implements Runnable {
-    private final RouterContext ctx;
-    private final Supplier<String> versionSupplier;
-    private final Supplier<File> fileSupplier;
-    private final Log _log;
+  private final RouterContext ctx;
+  private final Supplier<String> versionSupplier;
+  private final Supplier<File> fileSupplier;
+  private final Log _log;
 
-    WinUpdateProcess(RouterContext ctx, Supplier<String> versionSupplier, Supplier<File> fileSupplier) {
-        this.ctx = ctx;
-        this.versionSupplier = versionSupplier;
-        this.fileSupplier = fileSupplier;
-        this._log = ctx.logManager().getLog(WinUpdateProcess.class);
+  WinUpdateProcess(RouterContext ctx, Supplier<String> versionSupplier,
+                   Supplier<File> fileSupplier) {
+    this.ctx = ctx;
+    this.versionSupplier = versionSupplier;
+    this.fileSupplier = fileSupplier;
+    this._log = ctx.logManager().getLog(WinUpdateProcess.class);
+  }
+
+  private File workDir() throws IOException {
+    if (ctx != null) {
+      File workDir =
+          new File(ctx.getConfigDir().getAbsolutePath(), "i2p_update_win");
+      if (workDir.exists()) {
+        if (workDir.isFile())
+          throw new IOException(workDir +
+                                " exists but is a file, get it out of the way");
+        return workDir;
+      } else {
+        workDir.mkdirs();
+      }
+      return workDir;
     }
+    return null;
+  }
 
-    private File workDir() throws IOException {
-        if (ctx != null) {
-            File workDir = new File(ctx.getConfigDir().getAbsolutePath(), "i2p_update_win");
-            if (workDir.exists()) {
-                if (workDir.isFile())
-                    throw new IOException(workDir + " exists but is a file, get it out of the way");
-                return workDir;
-            } else {
-                workDir.mkdirs();
-            }
-            return workDir;
-        }
-        return null;
+  private void runUpdateInstaller() throws IOException {
+    String version = versionSupplier.get();
+    File file = fileSupplier.get();
+    if (file == null)
+      return;
+
+    var workingDir = workDir();
+    var logFile = new File(workingDir, "log-" + version + ".txt");
+
+    if (logFile.canWrite()) {
+      // check if we can write to the log file. If we can, use the
+      // ProcessBuilder to run the installer.
+      ProcessBuilder pb = new ProcessBuilder(
+          file.getAbsolutePath(), "/S", "/D=" + workingDir.getAbsolutePath());
+      var env = pb.environment();
+      env.put("OLD_I2P_VERSION", version);
+      env.remove("RESTART_I2P");
+
+      int exitCode = ctx.router().scheduledGracefulExitCode();
+      if (exitCode == Router.EXIT_HARD_RESTART ||
+          exitCode == Router.EXIT_GRACEFUL_RESTART)
+        env.put("RESTART_I2P", "true");
+
+      try {
+        pb.directory(workingDir)
+            .redirectErrorStream(true)
+            .redirectOutput(logFile)
+            .start();
+      } catch (IOException ex) {
+        _log.error(
+            "Unable to run update-program in background. Update will fail.");
+      }
+    } else {
+      // If we cant write to the log file and we're on Windows, use the elevator
+      // to execute the installer instead of the ProcessBuilder.
+      Elevator.executeAsAdministrator(file.getAbsolutePath(),
+                                      " /S /D=" + workingDir.getAbsolutePath());
     }
+  }
 
-    private void runUpdateInstaller() throws IOException {
-        String version = versionSupplier.get();
-        File file = fileSupplier.get();
-        if (file == null)
-            return;
-
-        var workingDir = workDir();
-        var logFile = new File(workingDir, "log-" + version + ".txt");
-
-        if (logFile.canWrite()) {
-            // check if we can write to the log file. If we can, use the ProcessBuilder to
-            // run the installer.
-            ProcessBuilder pb = new ProcessBuilder(file.getAbsolutePath(), "/S", "/D=" + workingDir.getAbsolutePath());
-            var env = pb.environment();
-            env.put("OLD_I2P_VERSION", version);
-            env.remove("RESTART_I2P");
-
-            int exitCode = ctx.router().scheduledGracefulExitCode();
-            if (exitCode == Router.EXIT_HARD_RESTART || exitCode == Router.EXIT_GRACEFUL_RESTART)
-                env.put("RESTART_I2P", "true");
-
-            try {
-                pb.directory(workingDir).redirectErrorStream(true).redirectOutput(logFile).start();
-            } catch (IOException ex) {
-                _log.error("Unable to run update-program in background. Update will fail.");
-            }
-        } else {
-            // If we cant write to the log file and we're on Windows, use the elevator to
-            // execute the installer instead of the ProcessBuilder.
-            Elevator.executeAsAdministrator(file.getAbsolutePath(), " /S /D=" + workingDir.getAbsolutePath());
-        }
-
+  @Override
+  public void run() {
+    try {
+      runUpdateInstaller();
+    } catch (IOException ioe) {
+      _log.error("Error running updater, update may fail." + ioe);
     }
-
-    @Override
-    public void run() {
-        try {
-            runUpdateInstaller();
-        } catch (IOException ioe) {
-            _log.error("Error running updater, update may fail." + ioe);
-        }
-    }
+  }
 }
--- java/net/i2p/router/WindowsUpdatePostProcessor.java
+++ java/net/i2p/router/WindowsUpdatePostProcessor.java
@@ -1,101 +1,100 @@
 package net.i2p.router;
 
+import static net.i2p.update.UpdateType.*;
+
 import java.io.*;
+import java.lang.InterruptedException;
+import java.lang.Process;
+import java.lang.ProcessBuilder;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.*;
-
-import net.i2p.crypto.*;
-import static net.i2p.update.UpdateType.*;
 import net.i2p.I2PAppContext;
-import net.i2p.update.UpdateType;
+import net.i2p.crypto.*;
 import net.i2p.update.UpdatePostProcessor;
+import net.i2p.update.UpdateType;
 import net.i2p.util.Log;
 import net.i2p.util.SystemVersion;
 
-import java.lang.ProcessBuilder;
-import java.lang.Process;
-import java.lang.InterruptedException;
-
 public class WindowsUpdatePostProcessor implements UpdatePostProcessor {
-    private final Log _log;
-    private final RouterContext ctx;
-    private final AtomicBoolean hook = new AtomicBoolean();
-    private final String fileName = "i2p-jpackage-update.exe";
+  private final Log _log;
+  private final RouterContext ctx;
+  private final AtomicBoolean hook = new AtomicBoolean();
+  private final String fileName = "i2p-jpackage-update.exe";
 
-    private volatile String version;
+  private volatile String version;
 
-    private volatile File positionedFile = null;
+  private volatile File positionedFile = null;
 
-    WindowsUpdatePostProcessor(RouterContext ctx) {
-        this.ctx = ctx;
-        this._log = ctx.logManager().getLog(WindowsUpdatePostProcessor.class);
+  WindowsUpdatePostProcessor(RouterContext ctx) {
+    this.ctx = ctx;
+    this._log = ctx.logManager().getLog(WindowsUpdatePostProcessor.class);
+  }
+
+  public String getVersion() { return version; }
+
+  public File getFile() { return positionedFile; }
+
+  public void updateDownloadedandVerified(UpdateType type, int fileType,
+                                          String version, File file)
+      throws IOException {
+    _log.info("Got an update to post-process");
+    if (SystemVersion.isWindows()) {
+
+      if (type != UpdateType.ROUTER_SIGNED_SU3 &&
+          type != UpdateType.ROUTER_DEV_SU3) {
+        _log.warn("Unsupported update type " + type);
+        return;
+      }
+
+      if (fileType != SU3File.TYPE_EXE) {
+        _log.warn("Unsupported file type " + fileType);
+        return;
+      }
+
+      this.positionedFile = moveUpdateInstaller(file);
+      this.version = version;
+
+      if (!hook.compareAndSet(false, true)) {
+        _log.info("shutdown hook was already set");
+        return;
+      }
+
+      _log.info("adding shutdown hook");
+
+      ctx.addFinalShutdownTask(
+          new WinUpdateProcess(ctx, this::getVersion, this::getFile));
     }
+  }
 
-    public String getVersion() {
-        return version;
-    }
-
-    public File getFile() {
-        return positionedFile;
-    }
-
-    public void updateDownloadedandVerified(UpdateType type, int fileType, String version, File file)
-            throws IOException {
-        _log.info("Got an update to post-process");
-        if (SystemVersion.isWindows()) {
-
-            if (type != UpdateType.ROUTER_SIGNED_SU3 && type != UpdateType.ROUTER_DEV_SU3) {
-                _log.warn("Unsupported update type " + type);
-                return;
-            }
-
-            if (fileType != SU3File.TYPE_EXE) {
-                _log.warn("Unsupported file type " + fileType);
-                return;
-            }
-
-            this.positionedFile = moveUpdateInstaller(file);
-            this.version = version;
-
-            if (!hook.compareAndSet(false, true)) {
-                _log.info("shutdown hook was already set");
-                return;
-            }
-
-            _log.info("adding shutdown hook");
-
-            ctx.addFinalShutdownTask(new WinUpdateProcess(ctx, this::getVersion, this::getFile));
-        }
-    }
-
-    private File moveUpdateInstaller(File file) throws IOException {
-        if (this.ctx != null) {
-            File newFile = new File(workDir(), fileName);
-            boolean renamedStatus = file.renameTo(newFile);
-            if (renamedStatus)
-                return newFile;
-            else
-                throw new IOException(
-                        "WindowsUpdatePostProcesssor unable to move file to working directory, update will fail");
-        }
+  private File moveUpdateInstaller(File file) throws IOException {
+    if (this.ctx != null) {
+      File newFile = new File(workDir(), fileName);
+      boolean renamedStatus = file.renameTo(newFile);
+      if (renamedStatus)
+        return newFile;
+      else
         throw new IOException(
-                "Router context not available to WindowsUpdatePostProcesssor, unable to find working directory, update will fail");
+            "WindowsUpdatePostProcesssor unable to move file to working directory, update will fail");
     }
+    throw new IOException(
+        "Router context not available to WindowsUpdatePostProcesssor, unable to find working directory, update will fail");
+  }
 
-    private File workDir() throws IOException {
-        if (this.ctx != null) {
-            File workDir = new File(this.ctx.getConfigDir().getAbsolutePath(), "i2p_update_win");
-            if (workDir.exists()) {
-                if (workDir.isFile())
-                    throw new IOException(workDir + " exists but is a file, get it out of the way");
-                return null;
-            } else {
-                workDir.mkdirs();
-            }
-            return workDir;
-        }
+  private File workDir() throws IOException {
+    if (this.ctx != null) {
+      File workDir =
+          new File(this.ctx.getConfigDir().getAbsolutePath(), "i2p_update_win");
+      if (workDir.exists()) {
+        if (workDir.isFile())
+          throw new IOException(workDir +
+                                " exists but is a file, get it out of the way");
         return null;
+      } else {
+        workDir.mkdirs();
+      }
+      return workDir;
     }
-
+    return null;
+  }
 }
