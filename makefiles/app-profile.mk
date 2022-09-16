e216b67105a4546876eb7f92695316f36ad6e5db
idk <hankhill19580@gmail.com>
2022-09-15 20:07:33 -0400

    remove user.js from profile makefiles

--- makefiles/app-profile.mk
+++ makefiles/app-profile.mk
@@ -1,4 +1,4 @@
-app-profile: .version build/app-profile/user.js build/app-profile/prefs.js build/app-profile/chrome/userChrome.css build/app-profile/bookmarks.html build/app-profile/storage-sync.sqlite
+app-profile: .version build/app-profile/user.js build/app-profile/prefs.js build/app-profile/chrome/userChrome.css build/app-profile/storage-sync.sqlite
 
 app-profile.tgz: app-profile
 #	$(eval PROFILE_VERSION := $(shell cat src/app-profile/version.txt))
@@ -6,26 +6,6 @@ app-profile.tgz: app-profile
 	sh -c 'ls build/I2P && cp -rv build/I2P build/app-profile/I2P'; true
 	install -m755 src/unix/i2pconfig.sh build/app-profile/i2pconfig.sh
 	cd build && tar -czf app-profile-$(PROFILE_VERSION).tgz app-profile && cp app-profile-$(PROFILE_VERSION).tgz ../
-
-src/app-profile/user.js:
-	wget -O src/app-profile/user.js "https://github.com/arkenfox/user.js/raw/master/user.js"
-	sed -i 's|user_pref("extensions.autoDisableScopes", 15);|user_pref("extensions.autoDisableScopes", 0);|g src/app-profile/user.js
-	sed -i 's|user_pref("extensions.enabledScopes", 5);|user_pref("extensions.enabledScopes", 1);|g' src/app-profile/user.js
-	sed -i 's|user_pref("dom.security.https_only_mode", true);|user_pref("dom.security.https_only_mode", false);|g' src/app-profile/user.js
-	
-
-build/app-profile/user.js: build/app-profile src/app-profile/user.js
-	cp src/app-profile/user.js build/app-profile/user.js
-	cp src/app-profile/user-overrides.js build/app-profile/user-overrides.js
-
-build/app-profile/prefs.js: build/app-profile src/app-profile/prefs.js
-	cp src/app-profile/prefs.js build/app-profile/prefs.js
-
-build/app-profile/chrome/userChrome.css: build/app-profile/chrome src/app-profile/chrome/userChrome.css
-	cp src/app-profile/chrome/userChrome.css build/app-profile/chrome/userChrome.css
-
-build/app-profile/bookmarks.html: build/app-profile src/app-profile/bookmarks.html
-	cp src/app-profile/bookmarks.html build/app-profile/bookmarks.html
 
 build/app-profile/storage-sync.sqlite: build/app-profile src/app-profile/storage-sync.sqlite
 	cp src/app-profile/storage-sync.sqlite build/app-profile/storage-sync.sqlite
--- makefiles/profile.mk
+++ makefiles/profile.mk
@@ -1,5 +1,5 @@
 
-profile: build/profile/user.js build/profile/prefs.js build/profile/bookmarks.html build/profile/storage-sync.sqlite
+profile: build/profile/user.js build/profile/prefs.js build/profile/storage-sync.sqlite
 
 profile.tgz: .version profile
 #	$(eval PROFILE_VERSION := $(shell cat src/profile/version.txt))
@@ -7,22 +7,6 @@ profile.tgz: .version profile
 	sh -c 'ls build/I2P && cp -rv build/I2P build/profile/I2P'; true
 	install -m755 src/unix/i2pbrowser.sh build/profile/i2pbrowser.sh
 	cd build && tar -czf profile-$(PROFILE_VERSION).tgz profile && cp profile-$(PROFILE_VERSION).tgz ../
-
-src/profile/user.js:
-	wget -O src/profile/user.js "https://github.com/arkenfox/user.js/raw/master/user.js"
-	sed -i 's|user_pref("extensions.autoDisableScopes", 15);|user_pref("extensions.autoDisableScopes", 0);|g src/profile/user.js
-	sed -i 's|user_pref("extensions.enabledScopes", 5);|user_pref("extensions.enabledScopes", 1);|g' src/profile/user.js
-	sed -i 's|user_pref("dom.security.https_only_mode", true);|user_pref("dom.security.https_only_mode", false);|g' src/profile/user.js
-
-build/profile/user.js: build/profile src/profile/user.js
-	cp src/profile/user.js build/profile/user.js
-	cp src/profile/user-overrides.js build/profile/user-overrides.js
-
-build/profile/prefs.js: build/profile src/profile/prefs.js
-	cp src/profile/prefs.js build/profile/prefs.js
-
-build/profile/bookmarks.html: build/profile src/profile/bookmarks.html
-	cp src/profile/bookmarks.html build/profile/bookmarks.html
 
 build/profile/storage-sync.sqlite: build/profile src/profile/storage-sync.sqlite
 	cp src/profile/storage-sync.sqlite build/profile/storage-sync.sqlite
