2f516f0c1133f939044db3a76c29aac20a711314
idk <hankhill19580@gmail.com>
2022-11-06 13:38:59 -0500

    Add zip.sh script to generate Windows portables

---
+++ zip.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+cd "$SCRIPT_DIR" || exit 1
+
+. "$SCRIPT_DIR/config.sh"
+
+if [ -f "$SCRIPT_DIR/config_override.sh" ]; then
+  . "$SCRIPT_DIR/config_override.sh"
+fi
+./clean.sh
+wsl make distclean
+./build.sh
+cd "$SCRIPT_DIR/I2P" || exit 1
+
+TORSOCKS=$(which torsocks)
+if [ -f "${TORSOCKS}" ]; then
+    . "${TORSOCKS}" on
+fi
+./lib/torbrowser-windows.sh
+version="$(curl -s https://aus1.torproject.org/torbrowser/update_3/release/downloads.json | jq -r ".version")"
+. "${TORSOCKS}" off
+locale="en-US" # mention your locale. default = en-US
+if [ -d /etc/default/locale ]; then
+    . /etc/default/locale
+    locale=$(echo "${LANG}" | cut -d . -f1)
+fi
+rm -vrf "tor-browser_${locale}"
+cd "$SCRIPT_DIR" || exit 1
+
+#tar czvf I2P.tar.gz I2P
