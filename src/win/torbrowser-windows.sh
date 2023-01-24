dd47ffd9023bc60b83740321e4b2caed1210cfc3
idk <hankhill19580@gmail.com>
2023-01-24 04:50:30 +0000

    don't use wine to unpack Tor Browser on Windows

--- src/win/torbrowser-windows.sh
+++ src/win/torbrowser-windows.sh
@@ -27,6 +27,12 @@ gpgv --keyring ./tor.keyring "torbrowser-install-win64-${version}_${locale}.exe.
 #tar xvJf "torbrowser-install-win64-${version}_${locale}.exe"
 #for n in `seq 1 2000`; do echo $n; dd ibs=256 if="torbrowser-install-win64-${version}_${locale}.exe" count=2 skip=$n | file - ; done 2>/dev/null |less
 #zip -FF "torbrowser-install-win64-${version}_${locale}.exe" --out extracted.zip
-export WINEPREFIX=$(pwd)/../tmp
-wine "torbrowser-install-win64-${version}_${locale}.exe" /S #/D .
-cp -vr "$WINEPREFIX/drive_c/users/idk/Desktop/Tor Browser/" "Tor Browser"
+WINE=$(which wine)
+if [ -z "$WINE" ]; then
+    "./torbrowser-install-win64-${version}_${locale}.exe" /S #/D .
+    cp -vr "$HOME/Desktop/Tor Browser/" "Tor Browser"
+else
+    export WINEPREFIX=$(pwd)/../tmp
+    $WINE "torbrowser-install-win64-${version}_${locale}.exe" /S #/D .
+    cp -vr "$WINEPREFIX/drive_c/users/idk/Desktop/Tor Browser/" "Tor Browser"
+fi
