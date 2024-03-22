0603ecf99a41a7082fcaab7a28bcedf0186e1841
eyedeekay <idk@mulder>
2024-03-22 14:14:41 -0400

    specify version based on tag for dev builds based on trunk routers

--- buildscripts/launcher.sh
+++ buildscripts/launcher.sh
@@ -58,14 +58,20 @@ if [ -z "$EXTRA" ]; then
 fi
 if [ "$VERSION" = master ]; then
   VERSIONDATE="$(date +%m%d)"
+  export CHECKOUT_VERSION="$VERSIONMAJOR.$VERSIONMINOR.$VERSIONBUILD"
+else
+  export CHECKOUT_VERSION="$VERSION"
 fi
+
+
+
 find . -name RouterVersion.java -exec sed -i "s|$OLDEXTRA|$EXTRA|g" {} \;
 git switch - || :
 git pull --tags
-git checkout -b "i2p-$VERSION$VERSIONDATE-$EXTRACODE" || :
-git commit -am "i2p-$VERSION$VERSIONDATE-$EXTRACODE" || :
-git archive --format=tar.gz --output="$SCRIPT_DIR/../i2p.firefox/i2p.i2p.jpackage-build.tar.gz" "i2p-$VERSION$VERSIONDATE-$EXTRACODE"
-git checkout "i2p-$VERSION$VERSIONDATE-$EXTRACODE" || :
+git checkout -b "i2p-$CHECKOUT_VERSION$VERSIONDATE-$EXTRACODE" || :
+git commit -am "i2p-$CHECKOUT_VERSION$VERSIONDATE-$EXTRACODE" || :
+git archive --format=tar.gz --output="$SCRIPT_DIR/../i2p.firefox/i2p.i2p.jpackage-build.tar.gz" "i2p-$CHECKOUT_VERSION$VERSIONDATE-$EXTRACODE"
+git checkout "i2p-$CHECKOUT_VERSION$VERSIONDATE-$EXTRACODE" || :
 
 for i in $COUNT; do
   echo -n "$i...."; sleep 1s
